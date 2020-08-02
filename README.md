e3526d088a4a1a6dce9070558606aaed00ce5cb3
Jim Knowler <jim.knowler2@gmail.com>
2020-08-02 09:32:38 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# spike-uart
+
+Spiking hardware implementation of UART serial comms.
+
+## requirements
+
+- python3
+
+- pyserial
+  > pip3 install pyserial
+
+- USB / serial cable
+  - https://www.amazon.co.uk/gp/product/B01N4X3BJB/ref=ppx_yo_dt_b_asin_title_o01_s00?ie=UTF8&psc=1
+
+## Scan for serial port
+
+> python3 -m serial.tools.list_ports
+
---
+++ uart.py
@@ -0,0 +1,76 @@
+import serial
+import time
+
+KNIGHTRIDER_TIME_INTERVAL = 0.5         # seconds
+
+def test_write_from_console(ser):
+    # read from console, write to port
+    while True:
+        c = input()
+        ser.write(str.encode(c))
+
+def test_write_counter(ser):
+    # write to port
+    line_count = 0
+
+    while True:
+        line_count += 1
+
+        print(line_count, ':', end='')
+        
+        for i in range(20):
+            c = str(i)
+            print(c + ', ', end='')
+            ser.write(str.encode(c))
+        print('')
+
+def test_write_knightrider(ser):
+    # write to port
+
+    while True:        
+        for i in range(0, 7):
+            c = str(1 << i)
+            print(c)
+            ser.write(str.encode(c))
+            #time.sleep(KNIGHTRIDER_TIME_INTERVAL)   
+
+        for i in reversed(range(1, 8)):
+            c = str(1 << i)
+            print(c)
+            ser.write(str.encode(c))
+            #time.sleep(KNIGHTRIDER_TIME_INTERVAL)
+
+def test_read(ser):
+    # read from port, write to console
+    line_count = 0
+
+    while True:
+        for i in range(64):
+            c = ser.read()
+            try:
+                c = c.decode('ascii')
+            except:
+                c='?'
+
+            print(c, end='')
+        print('')
+        line_count += 1
+
+        print(line_count, ':', end='')
+
+def main():
+
+    with serial.Serial('/dev/cu.usbserial-14320') as ser:
+        ser.baudrate = 115200
+        ser.stopbits = serial.STOPBITS_ONE
+        ser.parity = serial.PARITY_NONE
+        ser.bytesize = serial.EIGHTBITS
+        ser.xonxoff = False
+
+        print('port is_open:', ser.is_open)
+        
+        #test_write_knightrider(ser)
+        test_write_from_console(ser)
+        #test_read(ser)
+    
+main()
